17735045575a2a6cfda17c6657fc2172e9971687
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-04-21 16:47:53 +0300

    prime solved

--- IsPrime/is_prime.php
+++ IsPrime/is_prime.php
@@ -3,7 +3,6 @@ Is Prime?
 Write a php function called is_prime that checks the number if it's prime or not!
 If number is prime print: "The number (X) is prime", if not print: "number (X) is not prime".
 -->
-<!DOCTYPE html>
 <html>
 <head>
 	<title>php-toy-problem</title>
@@ -13,19 +12,27 @@ If number is prime print: "The number (X) is prime", if not print: "number (X) i
 	<?php
 
 	function is_prime($n) {
+		$flag = false;
 		if ($n === 1) {
-			echo "Number 1 is not Prime <br>";
+			echo "Number $n is not Prime <br>";
 		}
-		for ($i = 2; $i <$n ; $i++) { 
-			if ($n % $i === 0) {
-				echo "Number $n is not Prime <br>";
+		elseif ($n > 1) {
+			for ($i = 2; $i <$n ; $i++) { 
+				if ($n % $i === 0) {
+					$flag = true;
+				}
 			}
-			elseif (!$n % $i === 0) {
+			if (!$flag) {
 				echo "Number $n is Prime <br>";
+			}
+			elseif ($flag) {
+				echo "Number $n is not Prime <br>";
 			}
 		}
 	}
+
 	is_prime(1);
+	is_prime(2);
 	is_prime(3);
 	is_prime(5);
 	is_prime(6);
@@ -33,7 +40,30 @@ If number is prime print: "The number (X) is prime", if not print: "number (X) i
 	is_prime(8);
 	is_prime(9);
 	is_prime(10);
+	is_prime(11);
+	is_prime(12);
+	is_prime(13);
+	is_prime(14);
+	is_prime(15);
 	?>
+<!-- 
+	<script>
+		function isPrime (n) {
+			if (n === 1) {
+				return false;
+			}
+
+			for (var i = 2; i <= n; i++) {
+				if (n % i !== 0 || n === 2) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+
+	</script> -->
 </body>
 </html>
 
