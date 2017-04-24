980eea755fd2fc908c83c3c76ce8a8fb3d866aa5
rwaida <rwaidaAlmehanni>
2017-04-24 14:40:47 +0300

    done

--- MilitaryTime/toMilitary.php
+++ MilitaryTime/toMilitary.php
@@ -1,13 +1,44 @@
 
-/*
-Is Prime?
-Write a php function called is_prime that checks the number if it's prime or not!
-If number is prime print: "The number (X) is prime", if not print: "number (X) is not prime".
- */
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
 
+<body>
+<form method="post">
+<input type="text" name="time">
+<input type="submit" name="but">
+</form>
+<?php
+function toMiller(){
 
- function is_prime($n){
-   // code goes here
+  $time=$_POST['time'];
+  $am=strpos($time,'am');
+  $pm=strpos($time,'pm');
+  $d=strpos($time,':');
+  $part=substr($time,0,$d);
+ if($pm !== false && $part!==12){
+     $x=$part+12;
+     $pm2=substr($time,$d);
+     echo $x.$pm2;
+     die();
+      }
+else if($am !== false){
+      if(substr($time,0,2) !== 12){
+      echo "0". substr($time,0,$am);
+      die();
+     }else{
+       echo "00".substr($time,$d,$am);
+       die();
+     }
+}
+echo substr($time,0,5);
+}
+if(isset($_POST['but'])){
+	toMiller();
+}
+?>
 
-
- }
+</body>
+</html>
