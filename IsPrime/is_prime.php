d718e6bb6aebde9456045297b36099a68f9aa6b5
Admin <m2ghanom@gmail.com>
2017-04-24 12:22:43 +0300

    military

--- IsPrime/is_prime.php
+++ IsPrime/is_prime.php
@@ -1,13 +1,45 @@
 
-/*
-Is Prime?
+
+<!-- Is Prime?
 Write a php function called is_prime that checks the number if it's prime or not!
 If number is prime print: "The number (X) is prime", if not print: "number (X) is not prime".
- */
+ 
 
 
  function is_prime($n){
    // code goes here
 
+var_dump='lala';
 
+ } -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+
+
+echo "Hello World!<br>";
+
+
+
+
+function is_prime($n){
+ 	for ($x=2 ; $x<$n ; $x++) {
+    
+    if($n%$x == 0){
+    	echo  "is not prime ";
+    	break;
+    }else {
+ 		echo "is  prime ";
+ 	}
  }
+}
+ is_prime(4);
+
+ ?>
+</body>
+</html>
--- MilitaryTime/toMilitary.php
+++ MilitaryTime/toMilitary.php
@@ -1,13 +1,34 @@
+<?php 
+echo "ola<br>";
 
-/*
-Is Prime?
-Write a php function called is_prime that checks the number if it's prime or not!
-If number is prime print: "The number (X) is prime", if not print: "number (X) is not prime".
- */
+function military_time($time){
+	$t=0;
+	$num= intval($time);
+	if(strrpos($time, "pm") || strrpos($time, "am")){
+		$x =substr($time,0,strlen($time)-2);
+		///to get the 04:00  with out the pm or am ;
+		//echo $x;
+		if(strrpos($time, "pm") && $num !== 12){
+			$t=12+$num;
+		}else if($num == 12 &&strrpos($time, "am")){
+			$t=0;
+		}else{
+			$t=$num;
+		}
+		if ($t<10){
+			$t="0".$t;
+		}
+		echo str_replace($num, $t, $x);
+	}else{
+		echo $time;
+	}
+}
 
 
- function is_prime($n){
-   // code goes here
+military_time('10:15am'); 
 
 
- }
+
+
+
+ ?>
